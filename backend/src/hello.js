bf63da2be982e4fa0b1c5ab1363ec5056120c5c3
Tim <kristoft.1329@gmail.com>
2024-04-21 11:30:15 +0300

    Remove temp file

--- backend/src/hello.js
+++
@@ -1,9 +0,0 @@
-import * as db from './db.js'
-
-const main = async () => {
-    await db.updateTokenIdFor(20, '0x8203961DDE79eFB445b9705587a915328f3cb72e')
-    const record = await db.getRecordForToken(20)
-    console.log(record)
-}
-
-main()
